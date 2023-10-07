898e58c49c4c31d11a0bee25f3b2b0766e9e5369
Zusolaris <zusolaris.personal@gmail.com>
2023-10-07 15:30:51 -0700

    stuff

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,23 @@ function App() {
 
   return (
     <>
-      <h2>Welcome to [AppName]</h2>
-      <p>[AppName] helps you explore factors impacting climate change in your area, and take action to help preserve your environment.</p>
+      <h2>Welcome to (Appname) </h2>
+      <p>Green Haus helps you explore factors impacting climate change in your area, and take action to help preserve your environment.</p>
       <ul>
-        <li>Learn about NASA's EMIT mission to improve climate models and measure greenhouse gases</li>
-        <li>Learn about missions by other space agencies around the world investigating Earth's climate</li>
+    
+          <h3/>Learn about NASA's EMIT mission to improve climate models and measure greenhouse gases
+        The EMIT instrument is a NASA-developed imaging spectrometer used in space missions. It measures light in visible and infrared wavelengths to determine the mineral composition of surfaces. By capturing over 100,000 spectra per second, it creates detailed maps of Earth's mineral composition.
+      <h3/>
+      <img id="EMIT" src="./src/assets/EMIT IRL.png"></img>
+      <img id="EMIT" src="./src/assets/EMIT itself.png"></img>
+
+        <h2>Here is a worldview of YOUR HOME. Compare climate trends throughout the past year.</h2>
+
+
         <li>Explore your local climate including greenhouse gas sources, air quality, [EPA stuff?]</li>
+        
         <li>EcoHacks</li>
+
         <li>Advocacy</li>
       </ul>
       <hr />
